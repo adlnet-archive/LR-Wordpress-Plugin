0118aae4c0dcbb01848a8772eae87f60db4ef982
Mick Muzac <mmuzac23@gmail.com>
2013-02-28 10:38:22 -0500

    Two column subject map, IE8 fixes

--- widgets/templates/LRInterfaceSubjectTemplate.php
+++ widgets/templates/LRInterfaceSubjectTemplate.php
@@ -1,23 +1,22 @@
 <script type="text/html" id="subject-template">
-	<span>
-		
-		<!-- ko if: children.length > 0 -->
-			<a href="#" data-bind="text: '[ + ] '" class="standard-plus" />
-			<a href="#" data-bind="text: name" class="standard-link" /><br/><br/>
-			<div class="saveOpen"></div>
-			<div style="padding-left: 50px;" data-bind="template:{name: 'subject-template', foreach: children}, attr:{class:'standard-div standard-' + children.length }"/>
-		<!-- /ko -->
-		
-		<!-- ko if: children.length == 0 -->
-			<a href="#" data-bind="text: name" class="standard-link" />	<br/><br/>
-			<div class="noChildren"></div>
-		<!-- /ko -->
-		
-		
-	</span>
+		<div>
+			<!-- ko if: children.length > 0 -->
+				<a href="#" data-bind="text: '[ + ] '" class="standard-plus"></a>
+				<a href="#" data-bind="text: name" class="standard-link"></a><br/><br/>
+				<div class="saveOpen"></div>
+				<div style="padding-left: 40px;" data-bind="'template':{'name': 'subject-template', 'foreach': children}, 'attr':{'class':'standard-div standard-' + children.length}"></div>
+			<!-- /ko -->
+			
+			<!-- ko if: children.length == 0 -->
+				<a href="#" data-bind="text: name" class="standard-link"></a><br/><br/>
+				<div class="noChildren"></div>
+			<!-- /ko -->
+		</div>
 </script>
-
-<span id="subject-map" data-bind="template:{name: 'subject-template', foreach: children}"/>
+<div style="clear:both; overflow:hidden; margin: 25px auto; width: 100%;">
+	<div id="subject-map-left" style="width: 40%; float: left; padding-left:10%;" data-bind="'template':{'name': 'subject-template', 'foreach': children.slice(0, children.length * .5)}"></div>
+	<div id="subject-map-right" style="width: 40%; float: right;" data-bind="'template':{'name': 'subject-template', 'foreach': children.slice(children.length * .5, children.length)}"></div>
+</div>
 <script type="text/javascript">
 	
 	
@@ -27,19 +26,24 @@
 		$.getJSON(serviceHost + "/data/sitemap", function(data){
 
 			self.children = data.children;
-			ko.applyBindings(self, $("#subject-map")[0]);
+			self.totalRootSections = data.children.length;
+			self.currentLevel = 0;
+			self.currentCount = 1;
+			ko.applyBindings(self, $("#subject-map-left")[0]);
+			ko.applyBindings(self, $("#subject-map-right")[0]);
 
 
 			$(".standard-div").hide();
 			$(".standard-link, .standard-plus").click(function(e){
 				
 				e.preventDefault();
+				e.stopPropagation();
 				
 				//This element has no children.. start search
 				if($(this).siblings(".noChildren").length == 1){
 				
 					window.location.href = '<?php echo add_query_arg("query", "LRreplaceMe", get_page_link( $instance['results']));?>'.replace("LRreplaceMe", $(this).text());
-					return;
+					return false;
 				}
 				
 				var isOpen = $(this).siblings(".saveOpen").data("isOpen");
@@ -62,6 +66,8 @@
 					$(this).parent().children(".standard-plus").text("[ - ] ");
 					$(this).siblings(".standard-div").show();
 				}
+				
+				return false;
 			});
 		});
 	});	
