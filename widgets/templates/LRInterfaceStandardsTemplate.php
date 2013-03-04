01937436ba4b0d0a195d7e0fb39858477aadd570
Mick Muzac <mmuzac23@gmail.com>
2013-03-04 16:53:48 -0500

    Standards browser in progress widget

--- widgets/templates/LRInterfaceStandardsTemplate.php
+++ widgets/templates/LRInterfaceStandardsTemplate.php
@@ -2,29 +2,25 @@
 		<div>
 			<!-- ko if: $data && $data.children !== undefined -->
 				<a href="#" data-bind="text: '[ + ] '" class="standard-plus"></a>
-				<a href="#" data-bind="html: title[0].toUpperCase() + title.slice(1, title.length)" class="standard-link"></a>
-				<span data-bind="text: $data.count >= 0?$data.count : ''"></span>
+				<a href="#" data-bind="html: title[0].toUpperCase() + title.slice(1, title.length), attr:{name:$data.id!=undefined?$data.id:title}" class="standard-link"></a>
+				<span class="childrenResourceNumber" data-bind="text: $data.count >= 0? '( ' + $data.count + ' )': ''"></span>
 				<br/><br/>
 				<div class="saveOpen"></div>
 				<div style="padding-left: 40px;" data-bind="'template':{'name': 'standards-template', 'foreach': children}, 'attr':{'class':'standard-div standard-' + $data.children.length}"></div>
 			<!-- /ko -->
 			
 			<!-- ko if: $data && $data.children == undefined -->
-				<a href="#" data-bind="html: title" class="standard-link"></a>
+				<a href="#" data-bind="html: title, attr:{name: id}" class="standard-link"></a>
 				<br/><br/>
 				<div class="noChildren"></div>
 			<!-- /ko -->
 		</div>
 </script>
 
-<div style="clear:both; overflow:hidden; margin: 0 auto; width: 100%;">
+<div id="standardsMapContainer" class="loading" style="clear:both; overflow:hidden; margin: 0 auto; width: 100%;">
 	<div id="standards-map-left" style="width: 90%; float: left; padding-left:10%;" data-bind="'template':{'name': 'standards-template', 'data': standards[0]}"></div>
 	<div id="standards-map-right" style="width: 90%; float: left; padding-left:10%;" data-bind="'template':{'name': 'standards-template', 'data': standards[1]}"></div>
 </div>
-
-<div id="standardsMapContainer" style="clear:both; overflow:hidden; margin-bottom: 20px; width: 100%;"></div>
-
-Test
 
 	<script type="text/javascript">
 		
@@ -42,13 +38,27 @@ Test
 			spinner.spin($("#standardsMapContainer")[0]);
 			
 			$.getJSON(serviceHost + "/standards", function(data){
-
+				
+				$("#standardsMapContainer").removeClass("loading");
+				spinner.stop();
+				
 				self.standards = data;
 				
 				console.log(data, self);
 				ko.applyBindings(self, $("#standards-map-left")[0]);
 				ko.applyBindings(self, $("#standards-map-right")[0]);
 				$(".standard-div").hide();
+				
+				
+				$("#standardsMapContainer .standard-link").click(function(e){
+			
+					e.preventDefault();
+					e.stopPropagation();
+					
+					window.location.href = 
+					'<?php echo add_query_arg("query", "LRreplaceMe", get_page_link( $instance['results']));?>'.replace("LRreplaceMe", encodeURIComponent($(this).attr("name")));
+						return false;
+				});
 				
 			});
 		});
