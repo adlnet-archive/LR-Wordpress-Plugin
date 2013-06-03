d0db015cbb646f9f803baeb5af314a8c7d391d1a
Mick Muzac <mmuzac23@gmail.com>
2013-06-03 13:58:40 -0400

    Minor standards regression fix

--- widgets/templates/LRInterfaceStandardsTemplate.php
+++ widgets/templates/LRInterfaceStandardsTemplate.php
@@ -77,15 +77,13 @@
 			self.standards(saveStandardsData);
 			self.standards().loadChildren();
 			
-			//$("#standardsMapContainer .standard-div").hide();		
-			
 			console.log("DATA FORMAT: ", saveStandardsData);
 			spinner.stop();
 			standardCollapseAllAndOpen();
 		});			
 		
 		$("#standardsMapContainer").on("click",".standard-link-collapse",function(e){
-			//standardPlusCollapse(e, this);
+			standardPlusCollapse(e, this);
 		});
 
 		self.listOfStates(["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"]);
